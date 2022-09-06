3142fbe7cf8945445d96c804ecae70107722d154
xinghyfish <xinghy.fish@qq.com>
2022-09-06 23:00:55 +0800

    empty project

---
+++ src/test/java/com/xosmos/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xosmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
